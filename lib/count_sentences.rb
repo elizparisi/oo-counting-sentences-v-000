28764a9f96667e6b9b1b4db0a1b4b8f1f0caf5c7
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-25 05:56:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,6 @@ class String
   end
 
   def count_sentences
-    self.self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
